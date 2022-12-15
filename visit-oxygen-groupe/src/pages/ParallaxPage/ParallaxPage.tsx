fed01535d383d0d9e8b1d83e538479e5d8e393c1
flyether <flyether@gmail.com>
2022-12-15 23:35:32 +0300

    add cloud to  parallax for example

--- visit-oxygen-groupe/src/pages/ParallaxPage/ParallaxPage.tsx
+++ visit-oxygen-groupe/src/pages/ParallaxPage/ParallaxPage.tsx
@@ -57,6 +57,36 @@ const ParallaxPage = () => {
           </Link>
           <h1>05 Желтый</h1>
         </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.2 }}>
+            <img
+              alt="cloud"
+              src="https://freepngimg.com/thumb/cloud/7-cloud-png-image.png"
+              style={{ display: "block", width: "50%", marginLeft: "55%" }}
+            />
+            <img
+              alt="cloud"
+              src="https://freepngimg.com/thumb/cloud/7-cloud-png-image.png"
+              style={{ display: "block", width: "40%", marginLeft: "15%" }}
+            />
+          </ParallaxLayer>
+          <ParallaxLayer offset={2} speed={0} style={{ opacity: 0.6 }}>
+            <img
+              alt="cloud"
+              src="https://freepngimg.com/thumb/cloud/7-cloud-png-image.png"
+              style={{ display: "block", width: "50%", marginLeft: "10%" }}
+            />
+            <img
+              alt="cloud"
+              src="https://freepngimg.com/thumb/cloud/7-cloud-png-image.png"
+              style={{ display: "block", width: "40%", marginLeft: "55%" }}
+            />
+            <img
+              alt="cloud"
+              src="https://freepngimg.com/thumb/cloud/7-cloud-png-image.png"
+              style={{ display: "block", width: "30%", marginRight: "55%" }}
+            />
+          </ParallaxLayer>
       </Parallax>
     </div>
   );

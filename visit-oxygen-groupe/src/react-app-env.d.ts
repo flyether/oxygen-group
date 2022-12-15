bd4e04cac2abe7da98bc5564a9880fc6fa93d4f6
flyether <flyether@gmail.com>
2022-12-15 23:03:19 +0300

    fix: clear main

--- visit-oxygen-groupe/src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react-scripts" />
-declare module '*.png';
-declare module '*.svg';
-declare module '*.jpeg';
-declare module '*.jpg';
-declare module '*.mp3';
-declare module "*.module.css";
-declare module "*.module.scss";

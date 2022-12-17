9c312468b21334fa9e9c3f48648d2766e1e67665
flyether <flyether@gmail.com>
2022-12-17 18:29:04 +0300

    feat: change stracture

---
+++ visit-oxygen-groupe/src/components/Main/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Main.module.css'
+
+const Main = () => {
+
+  return (
+    <div className={styles.container}>
+      <h1>Тут текст с главной</h1>
+
+    </div>
+  );
+};
+
+export default Main;
--- visit-oxygen-groupe/src/pages/WelcomePage/WelcomePage.tsx
+++ visit-oxygen-groupe/src/pages/WelcomePage/WelcomePage.tsx
@@ -1,21 +1,18 @@
 import Header from "../../components/Header";
 import { Link } from 'react-router-dom';
 import styles from './WelcomePage.module.css'
-import ParallaxPage from "../../components/Parallax/Parallax";
+
 import Contacts from "../../components/Contacts";
+import ParallaxPage from "../../components/Parallax/paralaxComponenet";
+
+
+
 const WelcomePage = () => {
+ 
   return (
-    <main className={styles.container}>
-  
-   <div className={styles.divColum}>
-
-    <div className={styles.red}> <Header /></div>
-   <ParallaxPage />
-
-   <Contacts />
-   </div>
-   
-    </main>
+    
+    <ParallaxPage />
+ 
   );
 };
 
